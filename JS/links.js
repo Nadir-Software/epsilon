62b327802b0e2f065ce9fb8cbedec1a6c7b4475d
Nadir Technologies <116459177+Nadir-Software@users.noreply.github.com>
2023-05-24 20:54:31 +0100

    Add files via upload

--- JS/links.js
+++ JS/links.js
@@ -10,6 +10,15 @@ function openLink(site) {
     else if (site == "article") {
         if (localStorage.getItem('searchTerm') == null) {
             document.getElementById('wikipedia').blur();
+
+            document.getElementById('wikipedia').style.backgroundColor = "var(--error-container)";
+            document.getElementById('wikipedia').style.color = "var(--on-error-container)";
+
+            setTimeout(function() {
+                document.getElementById('wikipedia').style.backgroundColor = "";
+                document.getElementById('wikipedia').style.color = "";
+            }, 1000);
+
             return;
         }
 

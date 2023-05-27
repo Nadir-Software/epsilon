89a147b5ab40da300ab42003a22c6c6129bd8f26
Nadir Technologies <116459177+Nadir-Software@users.noreply.github.com>
2023-05-27 11:31:07 +0100

    Add files via upload

--- JS/copy.js
+++ JS/copy.js
@@ -4,15 +4,29 @@ function copy(content) {
     if (content == "article") {
         var contentToCopy = document.getElementById("response-text").innerText;
 
-        console.log(contentToCopy);
-
-        navigator.clipboard.writeText(contentToCopy);
-
-        copyButton.innerText = "done";
-
-        setTimeout(() => {
-            copyButton.innerText = "content_copy";
+        if (contentToCopy.includes("Text will appear here once you ask a question")) {
             copyButton.blur();
-        }, 1000);
+
+            copyButton.style.backgroundColor = "var(--error-container)";
+            copyButton.style.color = "var(--on-error-container)";
+
+            setTimeout(function() {
+                copyButton.style.backgroundColor = "";
+                copyButton.style.color = "";
+            }, 1000);
+        }
+
+        else {
+            console.log(contentToCopy);
+
+            navigator.clipboard.writeText(contentToCopy);
+
+            copyButton.innerText = "done";
+
+            setTimeout(() => {
+                copyButton.innerText = "content_copy";
+                copyButton.blur();
+            }, 1000);
+        }
     }
 }
